effe52ff2c1a72e1121cf7c6b65391939836270c
utkazu <hiroshimazeal@gmail.com>
2020-06-29 00:18:54 +0900

    [finish]SQL練習問題 問2

---
+++ sql_practice02.sql
@@ -0,0 +1,4 @@
+SELECT AVG(height) AS '平均身長', AVG(weight) AS '平均体重'
+FROM players
+WHERE position = 'GK'
+;
--- sql_practice1.sql
+++
@@ -1,4 +0,0 @@
-SELECT group_name AS 'グループ', MIN(ranking) AS 'ランキング最上位', MAX(ranking) AS 'ランキング最下位'
-FROM countries
-GROUP BY group_name
-;
